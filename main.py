4a330e8cd9812614c7938bb0676c2962c27a2ea2
Quality Gatekeeper <qgintest@gmail.com>
2025-05-15 22:38:22 -0400

    read xlsx files into python

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fpdf import FPDF
 import pandas as pd
+import glob
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(False, margin=0)
+filepaths = glob.glob("invoices/*.xlsx")
 
-df = pd.read_csv("invoices/10001-2023.1.18.xlsx")
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
+
+# pdf = FPDF(orientation="P", unit="mm", format="A4")
+# pdf.set_auto_page_break(False, margin=0)
+#
+# df = pd.read_csv("invoices/10001-2023.1.18.xlsx")

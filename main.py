1d28bd0f4582c3b246f374e4ac3e3880a33b456e
Quality Gatekeeper <qgintest@gmail.com>
2025-05-16 23:17:05 -0400

    create pdf from excel invoices

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fpdf import FPDF
 import pandas as pd
 import glob
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoiceNumber = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoiceNumber}")
+
+    pdf.output(f"PDFs/{filename}.pdf")
 
 # pdf = FPDF(orientation="P", unit="mm", format="A4")
 # pdf.set_auto_page_break(False, margin=0)

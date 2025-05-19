7e876e6abd5a492c09face15f2224015ca18b69f
Quality Gatekeeper <qgintest@gmail.com>
2025-05-19 16:38:45 -0400

    add header, total prices

--- main.py
+++ main.py
@@ -42,6 +42,22 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
 
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=33, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The Total Price is: {total_sum}", ln=1)
+
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 # pdf = FPDF(orientation="P", unit="mm", format="A4")
